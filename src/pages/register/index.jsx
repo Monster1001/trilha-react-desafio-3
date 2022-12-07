3642aad41ee084ab7cce0b8f61e38f7f4620f852
Max Lopes <maxlopes365@gmail.com>
2022-12-07 10:24:23 +0000

    TelaDeRegistro

--- src/pages/register/index.jsx
+++ src/pages/register/index.jsx
@@ -8,9 +8,9 @@ import { api } from '../../services/api';
 import { useForm } from "react-hook-form";
 
 
-import { Container, Title, Column, TitleLogin, SubtitleLogin, EsqueciText, CriarText, Row, Wrapper } from './styles';
+import { Container, Title, Column, TitleLogin, SubtitleLogin,  CriarText, Row, Wrapper, JaTenhoContaText, FazerLoginText } from './styles';
 
-const Login = () => {
+const Register = () => {
 
     const navigate = useNavigate()
 
@@ -33,35 +33,40 @@ const Login = () => {
             //TODO: HOUVE UM ERRO
         }
     };
-
+    
     console.log('errors', errors);
 
     return (<>
         <Header />
         <Container>
             <Column>
-                <Title>A plataforma para você aprender com experts, dominar as principais tecnologias
-                 e entrar mais rápido nas empresas mais desejadas.</Title>
+                <Title>A plataforma para você<br/> aprender com experts, dominar as principais tecnologias
+                 e entrar<br/> mais rápido nas<br/> empresas mais<br/> desejadas.</Title>
             </Column>
             <Column>
                 <Wrapper>
-                <TitleLogin>Faça seu cadastro</TitleLogin>
-                <SubtitleLogin>Faça seu login e make the change._</SubtitleLogin>
+                <TitleLogin>Comece agora grátis</TitleLogin>
+                <SubtitleLogin>Crie sua conta e make the change._</SubtitleLogin>
                 <form onSubmit={handleSubmit(onSubmit)}>
-                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="email"  control={control} />
+                    <Input placeholder="Nome completo" leftIcon={<MdEmail />} name="NomeCompleto"  control={control} />
+                    <Input placeholder="E-mail" leftIcon={<MdEmail />} name="E-mailCadastro"  control={control} />
                     {errors.email && <span>E-mail é obrigatório</span>}
-                    <Input type="password" placeholder="Senha" leftIcon={<MdLock />}  name="senha" control={control} />
+                    <Input type="password" placeholder="Password" leftIcon={<MdLock />}  name="senha" control={control} />
                     {errors.senha && <span>Senha é obrigatório</span>}
-                    <Button title="Entrar" variant="secondary" type="submit"/>
+                    <Button title="Criar minha conta" variant="secondary" type="submit"/>
                 </form>
                 <Row>
-                    <EsqueciText>Esqueci minha senha</EsqueciText>
-                    <a href="/register" style={{ textDecoration: "none",}}><CriarText>Criar Conta</CriarText></a>
+                    <CriarText>Ao clicar em "criar minha conta grátis", declaro que aceito as Políticas de Privacidade e os Termos de Uso da DIO.</CriarText>
+                    
+                    
                 </Row>
+                    <JaTenhoContaText>Já tenho conta.</JaTenhoContaText>
+                    
                 </Wrapper>
+                    <a href="/login" style={{ textDecoration: "none",}}><FazerLoginText>Fazer login</FazerLoginText></a>
             </Column>
         </Container>
     </>)
 }
 
-export { Login }
+export { Register }
